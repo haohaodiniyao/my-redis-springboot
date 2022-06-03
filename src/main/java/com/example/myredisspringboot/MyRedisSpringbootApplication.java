a7a678567b23a92feb4186691a8f96ec4a0d7097
haohaodiniyao <1270305593@qq.com>
2022-06-03 12:29:25 +0800

    Initial commit

---
+++ src/main/java/com/example/myredisspringboot/MyRedisSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.myredisspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRedisSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRedisSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myredisspringboot/MyRedisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myredisspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRedisSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
